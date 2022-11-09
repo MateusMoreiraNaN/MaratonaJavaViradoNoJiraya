592174ccace5f2d0ebbaf5bf784474881d399dab
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-11-09 15:56:37 -0300

    break

---
+++ Maratona_Java/src/academy/devdojo/maratonaJava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula06EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        int valorMax = 50;
+
+        for(int i = 0; i <= valorMax; i++){
+            if(i > 25){
+                break;
+                // break () switch or loop (for)
+            }
+            System.out.println("i = " + i);
+
+
+        }
+    }
+}
