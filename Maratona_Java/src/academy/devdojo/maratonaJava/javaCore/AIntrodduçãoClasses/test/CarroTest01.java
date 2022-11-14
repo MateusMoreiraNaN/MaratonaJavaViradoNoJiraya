d2f6f853e5bb6e6fb617b9f0f8d7da71ac906a47
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-11-14 19:17:12 -0300

    classes

--- Maratona_Java/src/academy/devdojo/maratonaJava/javaCore/AIntrodduçãoClasses/test/CarroTest01.java
+++ Maratona_Java/src/academy/devdojo/maratonaJava/javaCore/AIntrodduçãoClasses/test/CarroTest01.java
@@ -5,10 +5,17 @@ import academy.devdojo.maratonaJava.javaCore.AIntrodduçãoClasses.dominio.Carro
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+        Carro carro2 = new Carro();
 
         carro.Nome = "Tesla";
         carro.Modelo = "Model 3";
         carro.Ano = 2019;
+
+        carro2.Nome = "Fusca";
+        carro2.Modelo = "Fusca Bala";
+        carro2.Ano = 1984;
+
+        carro = carro2;
 
         System.out.println("Nome: " + carro.Nome);
         System.out.println("Modelo: " + carro.Modelo);
