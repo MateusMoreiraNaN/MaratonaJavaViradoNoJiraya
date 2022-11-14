0e2c661b7f8cc9ff0aee2704822f8c3057c9e96f
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-11-14 12:50:55 -0300

    continue

---
+++ Maratona_Java/src/academy/devdojo/maratonaJava/introducao/Aula06EstruturasDeRepeticao05.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula06EstruturasDeRepeticao05 {
+    public static void main(String[] args) {
+
+        //int n = 0;
+
+        for(int i = 0; i <= 10; i++){
+            continue;
+            //System.out.println(i);
+
+        }
+    }
+}
